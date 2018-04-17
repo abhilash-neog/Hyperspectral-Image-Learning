ab9b7e233dcce9db8a2471d52c39ac710459b79c
Gautam Pathak <25124204+gPats@users.noreply.github.com>
2018-04-17 17:10:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap[] b = new Bitmap[9];
+            int img;
+            for (int i = 0; i < 9; i++)
+            {
+                img = 600 + i * 50;
+                b[i] = new Bitmap("C:\\Users\\admin\\Desktop\\Tongue_tumor\\" + img + "nm.png");
+                Console.WriteLine(b[i].Width + "--" + b[i].Height);
+            }
+            Console.ReadKey();
+
+     //       Color color = b1.GetPixel();
+        }
+    }
+}
