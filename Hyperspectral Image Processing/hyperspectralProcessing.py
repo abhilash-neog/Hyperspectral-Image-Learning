726ed5bb586b582798c15abd7c715b5a1137e91d
Abhilash <abhineog97@gmail.com>
2018-08-16 00:49:15 +0530

    updates

--- Hyperspectral Image Processing/hyperspectralProcessing.py
+++ Hyperspectral Image Processing/hyperspectralProcessing.py
@@ -8,4 +8,11 @@ Created on Wed Aug 15 20:40:38 2018
 from spectral import *
 
 img = open_image('92AV3C.lan')
-print(img.__class__)
+view = imshow(img,(100,100,100))
+gt = open_image('92AV3GT.GIS').read_band(0)
+view = imshow(classes=gt)
+
+view = imshow(img,(100,100,100),classes=gt)
+view.set_display_mode('overlay')
+view.class_alpha = 0.3
+save_rgb('gt.jpg', gt, colors=spy_colors)
