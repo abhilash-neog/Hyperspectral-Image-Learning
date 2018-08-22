ef9d920cd5c8a863b8bda8fd2e4c55a57217709a
Abhilash <abhineog97@gmail.com>
2018-08-22 18:45:44 +0530

    edits

--- HIS Classification using CNN/hsiClassifier.py
+++ HIS Classification using CNN/hsiClassifier.py
@@ -4,28 +4,33 @@ from keras.models import Sequential, Model
 
 img = open_image(r'C:\Users\user\Desktop\Abhilash\Imp\CEERI\NN\Hyperspectral Image Visualization\92AV3C.lan')
 
-"""
-img.shape
-Out[2]: (145, 145, 220)
+#img.shape
+#Out[2]: (145, 145, 220)
 
-img
-Out[3]: 
-	Data Source:   'C:\Users\user\Desktop\Abhilash\Imp\CEERI\NN\Hyperspectral Image Visualization\92AV3C.lan'
-	# Rows:            145
-	# Samples:         145
-	# Bands:           220
-	Interleave:        BIL
-	Quantization:  16 bits
-	Data format:     int16
+#img
+#Out[3]: 
+#	Data Source:   'C:\Users\user\Desktop\Abhilash\Imp\CEERI\NN\Hyperspectral Image Visualization\92AV3C.lan'
+#	# Rows:            145
+#	# Samples:         145
+#	# Bands:           220
+#	Interleave:        BIL
+#	Quantization:  16 bits
+#	Data format:     int16
     
-imshow(img)
-Out[4]: 
-ImageView object:
-  Display bands       :  [0, 110, 219]
-  Interpolation       :  <default>
-  RGB data limits     :
-    R: [2632.0, 4536.0]
-    G: [1017.0, 1159.0]
-    B: [980.0, 1034.0]
-"""
+#imshow(img)
+#Out[4]: 
+#ImageView object:
+#  Display bands       :  [0, 110, 219]
+#  Interpolation       :  <default>
+#  RGB data limits     :
+#    R: [2632.0, 4536.0]
+#    G: [1017.0, 1159.0]
+#    B: [980.0, 1034.0]
 
+#loading the complete image
+
+imgX = img.load()
+#print(imgX)
+
+#imgX.shape
+#Out[10]: (145, 145, 220)
