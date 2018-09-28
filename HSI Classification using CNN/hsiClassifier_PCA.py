1a4a27de0908852e6549fa61231b5ac97a40f037
gPats <f2016134@pilani.bits-pilani.ac.in>
2018-09-28 21:01:14 +0530

    PCA added- accuracy 51.16 PCA components = 50

--- HSI Classification using CNN/hsiClassifier_PCA.py
+++ HSI Classification using CNN/hsiClassifier_PCA.py
@@ -8,6 +8,9 @@ from keras import optimizers
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 import os
 
 
@@ -20,6 +23,7 @@ abs_file_path = os.path.join(script_dir, rel_path)
 
 
 img = open_image(abs_file_path)
+
 #img.shape
 #Out[2]: (145, 145, 220)
 
@@ -46,30 +50,26 @@ img = open_image(abs_file_path)
 #loading the complete image
 
 imgX = img.load()
-#print(imgX)
-
-#imgX.shape
-#Out[10]: (145, 145, 220)
 
 #the target features
 gt = sio.loadmat(r'C:\Users\admin\Hyperspectral-Image-Learning\HSI Classification using CNN\data\Indian_pines_gt.mat')
 gtd = gt['indian_pines_gt']#target
 
-#imgN = np.empty([21025,220,1])#feature vectors
-k = 0
-imgN = []
-#print(imgN.shape)
-for i in range(0,len(imgX)):
-    for j in range(0,imgX[i].shape[1]):
-        x1 = imgX[i,j,:].reshape(220,1)
-        imgN.append(x1)
-        #imgN[k] = imgX[i,j,:].reshape(220,1,1)#(1,220)
-        k+=1
-        
-Y = gtd.flatten()#feature labels
+Y = gtd.flatten()
 Y = list(Y)
-X_train, X_test, y_train, y_test = train_test_split(imgN,Y, test_size = 0.60)
-#X_train = list(X_train)
+
+imgX = imgX.reshape(145*145,220)
+
+imgX = StandardScaler().fit_transform(imgX)
+
+pca = PCA(n_components=50)
+
+principal_components = pca.fit_transform(imgX)
+
+principal_components = principal_components.reshape(145*145,50,1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(principal_components,Y, test_size = 0.60)
+
 def labelEncode(labels):
     #one_hot_labels = keras.utils.to_categorical(labels, num_classes=10)
     lab_encoder = LabelEncoder()
@@ -80,8 +80,8 @@ def labelEncode(labels):
     return onehot_encoded
 
 def get_model():
-    inputs = Input(shape=(220,1,1))
-    #model = Sequential()
+    inputs = Input(shape=(50,1,1))
+    model = Sequential()
     #"""
     x = Conv2D(20,kernel_size = (5,1), activation = 'tanh')(inputs)
     x = MaxPooling2D(pool_size = (3,1))(x)
@@ -98,11 +98,12 @@ def get_model():
     
     x = Dense(100,activation = 'tanh')(x)
     x = Dense(50, activation = 'tanh')(x)
+    
     output = Dense(17, activation = 'softmax')(x)
     model = Model(inputs=inputs, outputs=output)
     #"""
     """
-    model.add(Conv1D(20,kernel_size = 25, input_dim = 220))
+    model.add(Conv1D(20,kernel_size = 25, input_dim = 16))
     #Input size should be [batch, 1d, 2d, ch] = (None, 1, 15000, 1)
     model.add(Activation('tanh'))
     model.add(MaxPooling1D(pool_size = 6))
@@ -110,32 +111,46 @@ def get_model():
     model.add(Activation('tanh'))
     model.add(Dense(16))
     model.add(Activation('softmax'))
-    """
+    #"""
     return model
 
 y_train = labelEncode(y_train)
 y_test = labelEncode(y_test)
 model = get_model()
 
-sgd = optimizers.SGD(lr=0.5, decay=1e-6, momentum=0.9, nesterov=True)
+
+
+
+'''sgd = optimizers.SGD(lr=0.5, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer = sgd, loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-X_train = np.array(X_train).reshape(len(X_train),len(X_train[0]),len(X_train[0][0]),1)
-X_test = np.array(X_test).reshape(len(X_test),len(X_test[0]),len(X_test[0][0]),1)
 model.fit(np.array(X_train),y_train,epochs = 20, batch_size = 32)
 score = model.evaluate(np.array(X_test),y_test, batch_size = 32)
 
 #sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+'''
 ada = optimizers.Adagrad(lr=0.001, decay=1e-6)
 model.compile(optimizer = ada, loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-X_train = np.array(X_train).reshape(len(X_train),len(X_train[0]),len(X_train[0][0]),1)
-X_test = np.array(X_test).reshape(len(X_test),len(X_test[0]),len(X_test[0][0]),1)
-
-model.fit(np.array(X_train),y_train,epochs = 20, batch_size = 16)
+model.fit(np.array(X_train),y_train,epochs = 10, batch_size = 16)
 score = model.evaluate(np.array(X_test),y_test, batch_size = 16)
 
 print(score)
 #test accuracy - 58.565% -> improvement
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
