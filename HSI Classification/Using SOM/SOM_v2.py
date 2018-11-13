7e767761945bb97c505f394fb64c4543136b80d9
Abhilash <abhineog97@gmail.com>
2018-11-13 14:12:20 +0530

    SOM_v2 modified

--- HSI Classification/Using SOM/SOM_v2.py
+++ HSI Classification/Using SOM/SOM_v2.py
@@ -4,7 +4,8 @@ import os
 from spectral import *
 import scipy.io as sio
 import matplotlib.pyplot as plt
-import sys
+#import sys
+from sklearn.preprocessing import scale
 #from sklearn.decomposition import PCA
 #from sklearn.preprocessing import StandardScaler
 
@@ -21,17 +22,31 @@ img = open_image(abs_file_path)
 imgX = img.load()
 imgX = imgX.reshape(145*145,220)
 raw_data = imgX
-
-map_dim = 16
-
-som = MiniSom(map_dim, map_dim, 220, sigma=1.0, learning_rate=0.5,neighborhood_function='gaussian', random_seed=10)
+raw_data = scale(raw_data)
+map_dim = 145
+som = MiniSom(map_dim, map_dim, 220, sigma=4.0, learning_rate=0.5,neighborhood_function='gaussian')
 #som.random_weights_init(W)
 som.pca_weights_init(raw_data)
-som.train_random(raw_data, 100)
+print("Training...")
+som.train_random(raw_data, 1000)
+print("\n...ready!")
+
+plt.figure(figsize=(8, 8))
+wmap = {}
+im = 0
+for x, t in zip(raw_data, gtd):  # scatterplot
+    w = som.winner(x)
+    wmap[w] = im
+    plt. text(w[0]+.5,  w[1]+.5,  str(t),
+              color=plt.cm.rainbow(t / 16.), fontdict={'weight': 'bold',  'size': 11})
+    im = im + 1
+plt.axis([0, som.get_weights().shape[0], 0,  som.get_weights().shape[1]])
+plt.savefig('som_pines.png')
+plt.show()
 #qnt = som.quantization(raw_data)
 #print(qnt)
-
-max_iter = 10000
+"""
+max_iter = 500
 q_error_pca_init = []
 iter_x = []
 for i in range(max_iter):
@@ -43,8 +58,10 @@ for i in range(max_iter):
         q_error_pca_init.append(error)
         iter_x.append(i)
         sys.stdout.write(f'\riteration={i:2d} status={percent:0.2f}% error={error}')
-        
-        
+
 plt.plot(iter_x, q_error_pca_init)
 plt.ylabel('quantization error')
 plt.xlabel('iteration index')
+plt.savefig("testresults.pdf")
+"""
+
