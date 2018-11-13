39b771a54672c014ed000a5cc3d676965a18c7da
Abhilash <abhineog97@gmail.com>
2018-11-13 18:40:26 +0530

    new plots

--- HSI Classification/Using SOM/SOM_v2.py
+++ HSI Classification/Using SOM/SOM_v2.py
@@ -18,7 +18,6 @@ abs_file_path2 = os.path.join(script_dir,rel_path2)
 gt = sio.loadmat(abs_file_path2)
 gtd = gt['indian_pines_gt']
 gtd = gtd.flatten()
-
 img = open_image(abs_file_path)
 imgX = img.load()
 imgX = imgX.reshape(145*145,220)
@@ -26,22 +25,22 @@ raw_data = imgX
 def dimensionality_reduction(dat):
     #dat = dat.reshape(145*145,220)
     dat = StandardScaler().fit_transform(dat)
-    pca = PCA(n_components = 50, svd_solver='randomized',whiten=True)
+    pca = PCA(n_components = 100, svd_solver='randomized',whiten=True)
     print("explained_variance_ration:",sum(pca.fit(dat).explained_variance_ratio_))
     principal_components = pca.fit_transform(dat)
     
-    principal_components = principal_components.reshape(145*145,50)
+    principal_components = principal_components.reshape(145*145,100)
     
     return principal_components
 
 
 raw_data = dimensionality_reduction(raw_data)
-map_dim = 145
-som = MiniSom(map_dim, map_dim, 50, sigma=4.0, learning_rate=0.5,neighborhood_function='gaussian')
+map_dim = 50
+som = MiniSom(map_dim, map_dim, 100, sigma=4.0, learning_rate=0.5,neighborhood_function='gaussian')
 #som.random_weights_init(W)
 som.pca_weights_init(raw_data)
 print("Training...")
-som.train_random(raw_data, 1000)
+som.train_random(raw_data, 5000)
 print("\n...ready!")
 
 plt.figure(figsize=(16, 16))
@@ -51,7 +50,7 @@ for x, t in zip(raw_data, gtd):  # scatterplot
     w = som.winner(x)
     wmap[w] = im
     plt. text(w[0]+.5,  w[1]+.5,  str(t),
-              color=plt.cm.rainbow(t / 10.), fontdict={'weight': 'bold',  'size': 11})
+              color=plt.cm.rainbow(t / 16), fontdict={'weight': 'bold',  'size': 14})
     im = im + 1
 plt.axis([0, som.get_weights().shape[0], 0,  som.get_weights().shape[1]])
 #plt.savefig('som_pines.png')
